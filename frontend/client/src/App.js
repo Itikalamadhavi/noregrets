f83427ee9f0f0d6830b0dec09bc04ca184e50862
Madhavi Itikala <mitikala@innominds.com>
2021-11-08 13:13:06 +0530

    added

---
+++ frontend/client/src/App.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+import react from 'react';
+import Header from './components/header.js';
+import Body from './components/body.js';
+import Footer from './components/footer.js';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Body></Body>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/client/src/components/body.js
@@ -0,0 +1,9 @@
+import react from 'react';
+
+export default function Body() {
+    return(
+        <>
+        <h2>Body</h2>
+        </>
+    )
+}
